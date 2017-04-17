748ad7a249509518b51d3249083a028fa3036727
Jeremy Greer <jeremy.greer.atl@gmail.com>
2017-04-16 22:43:51 -0500

    first pass at picking out unique words

--- src/index.js
+++ src/index.js
@@ -1,12 +1,80 @@
+function getNextIndex(pattern, input, index) {
+  return input.indexOf(pattern, index + 1)
+}
+
 export function getIndicesOfPattern(pattern, input) {
   const indices = []
-  let index = -1
-  do {
-    index = input.indexOf(pattern, index + 1)
-    if (index > -1) {
-      indices.push(index)
-    }
-  } while (index > -1)
+  let index = getNextIndex(pattern, input, -1)
+  while (index > -1) {
+    indices.push(index)
+    index = getNextIndex(pattern, input, index)
+  }
   return indices
 }
 
+export function getMinimumDifference(numbers) {
+  const differences = []
+  numbers.forEach((n, i) => {
+    if (i > 0) {
+      differences.push(n - numbers[i - 1])
+    }
+  })
+  return Math.min.apply(null, differences)
+}
+
+export function getLongestWordWithMostIndices(data) {
+  const indexCounts = []
+  data.forEach(row => {
+    const count = row.indices.length
+    if (indexCounts.indexOf(count) < 0) {
+      indexCounts.push(count)
+    }
+  })
+  const requiredIndicesCount = Math.max.apply(null, indexCounts)
+
+  let currentWinner = null
+  data.forEach(row => {
+    if (row.indices.length === requiredIndicesCount) {
+      if (!currentWinner
+        || (row.word.length > currentWinner.word.length)) {
+        currentWinner = row
+      }
+    }
+  })
+  return currentWinner
+}
+
+// Returns unique string patterns (of at least length n), not necessarily
+// English words.
+export function getUniqueWords(input) {
+  const uniqueWords = []
+  const possibleWords = []
+  for (let i = 0; i < input.length; i++) {
+    let length = input.length - i
+    while (length > 1) {
+      const word = input.substr(i, length)
+      const indices = getIndicesOfPattern(word, input)
+      const minIndexDistance = getMinimumDifference(indices)
+      // To be a unique word, it must not have indices closer together than the
+      // word's length.
+      const overlapsWithOtherInstances = word.length > minIndexDistance
+      if (!overlapsWithOtherInstances) {
+        // Pick the longest word with the most indices.
+        possibleWords.push({
+          word,
+          indices,
+        })
+      }
+      length--
+    }
+    const repeatedWord = getLongestWordWithMostIndices(possibleWords)
+    if (repeatedWord.word !== input) {
+      if (uniqueWords.indexOf(repeatedWord.word) < 0) {
+        console.log({ repeatedWord })
+        uniqueWords.push(repeatedWord.word)
+      }
+    }
+  }
+  console.log(uniqueWords)
+  return uniqueWords
+}
--- src/index.test.js
+++ src/index.test.js
@@ -8,14 +8,54 @@ describe('finding repeated "words"', () => {})
 
 describe('recognizing string patterns', () => {
   describe('finding instances of a pattern', () => {
-    it('should return indices', () => {
-      const input = 'asdfasdfasdf'
-      const output = app.getIndicesOfPattern('as', input)
-      expect(output).toEqual([
-        0,
-        4,
-        8,
-      ])
+    describe('when there are matches', () => {
+      it('should return indices', () => {
+        const input = 'asdfasdfasdf'
+        const output = app.getIndicesOfPattern('as', input)
+        expect(output).toEqual([0, 4, 8])
+      })
+    })
+
+    describe('when there are no matches', () => {
+      it('should return []', () => {
+        const input = 'asdfasdfasdf'
+        const output = app.getIndicesOfPattern('AS', input)
+        expect(output).toEqual([])
+      })
+    })
+  })
+
+  describe('finding recurring strings', () => {
+    it('should return a list of all uniqe string combinations', () => {
+      // const input = 'abcabcabc'
+      const input = 'Hello, how are you?  I am OK.  Do you know what I did today?  I caught my first flounder!  I let him go because he was a bit too small.  Did you know they have big teeth.  It was surprising.'
+      const output = app.getUniqueWords(input)
+      expect(output).toEqual(['ll', ' h', '  '])
+    })
+  })
+
+  describe('getMinimumDifference', () => {
+    it('should return the smallest difference', () => {
+      const input = [0, 3, 6, 90, 134]
+      const output = app.getMinimumDifference(input)
+      expect(output).toBe(3)
+    })
+  })
+
+  describe('getLongestWordWithMostIndices', () => {
+    it('should return exactly what you think', () => {
+      const input = [
+        { word: 'abcabcabc', indices: [0] },
+        { word: 'abcabcab', indices: [0] },
+        { word: 'abcabca', indices: [0] },
+        { word: 'abc', indices: [0, 3, 6] },
+        { word: 'ab', indices: [0, 3, 6] },
+        { word: 'a', indices: [0, 3, 6] },
+      ]
+      const output = app.getLongestWordWithMostIndices(input)
+      expect(output).toEqual(
+        { word: 'abc', indices: [0, 3, 6] }
+      )
     })
   })
 })
