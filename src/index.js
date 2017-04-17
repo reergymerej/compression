37f24a176633d0fc5579aead89c5647a8e4dd0d8
Jeremy Greer <jeremy.greer.atl@gmail.com>
2017-04-16 21:41:51 -0500

    remove mocha & chai, write first index test

--- src/index.js
+++ src/index.js
@@ -1,2 +1,12 @@
-console.log('Hello, world.');
+export function getIndicesOfPattern(pattern, input) {
+  const indices = []
+  let index = -1
+  do {
+    index = input.indexOf(pattern, index + 1)
+    if (index > -1) {
+      indices.push(index)
+    }
+  } while (index > -1)
+  return indices
+}
 
--- src/index.test.js
+++ src/index.test.js
@@ -1,8 +1,21 @@
-import { expect } from 'chai';
-import app from '../src';
+import * as app from '../src'
 
-describe('sanity', () => {
-  it('should exist', () => {
-    expect('it').be.equal(true)
+  /*
+   * Look through text and find repeated "words".
+   * Replace repeated "words" with pointers to previous usage.
+   */
+describe('finding repeated "words"', () => {})
+
+describe('recognizing string patterns', () => {
+  describe('finding instances of a pattern', () => {
+    it('should return indices', () => {
+      const input = 'asdfasdfasdf'
+      const output = app.getIndicesOfPattern('as', input)
+      expect(output).toEqual([
+        0,
+        4,
+        8,
+      ])
+    })
   })
 })
